d7ecd992ceaad4d8401ea7f83da09d7e360c0e5c
Michel Sabchuk <michel@turbosys.com.br>
2020-03-03 11:36:33 -0300

    Use explicit requirements in the setup.py file.

--- setup.py
+++ setup.py
@@ -1,34 +1,21 @@
 """This module contains the packaging routine for the pybook package"""
 
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
 
+requirements = [
+    "scrapy>=1.0.0",
+    "selenium>=3.9.0",
+]
 
-def get_requirements(source):
-    """Get the requirements from the given ``source``
-
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
+test_requirements = [
+    "pytest==3.4.0",
+    "coverage<4.4",
+    "pytest-cov==2.4.0",
+    "codeclimate-test-reporter==0.2.3",
+    "attrs>=17.4.0",
+]
 
 setup(
     packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
+    install_requires=requirements,
 )
-
-
