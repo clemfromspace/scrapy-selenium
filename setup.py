218311ba87fdd49fcc40f6d9a1f49dec6bd2c7b2
Michel Sabchuk <michel@turbosys.com.br>
2020-03-03 15:06:44 -0300

    Call pytest when run python setup test.
    Also adjust tests_require instruction and don't stick to an specific
    pytest version.

--- setup.py
+++ setup.py
@@ -2,20 +2,19 @@
 
 from setuptools import setup, find_packages
 
-requirements = [
-    "scrapy>=1.0.0",
-    "selenium>=3.9.0",
-]
-
-test_requirements = [
-    "pytest==3.4.0",
-    "coverage<4.4",
-    "pytest-cov==2.4.0",
-    "codeclimate-test-reporter==0.2.3",
-    "attrs>=17.4.0",
-]
-
 setup(
     packages=find_packages(),
-    install_requires=requirements,
+    setup_requires=[
+        "pytest-runner",
+    ],
+    install_requires=[
+        "scrapy>=1.0.0",
+        "selenium>=3.9.0",
+    ],
+    tests_require=[
+        "pytest",
+        "coverage",
+        "pytest-cov",
+        "codeclimate-test-reporter",
+    ]
 )
