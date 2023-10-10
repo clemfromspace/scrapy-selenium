61bf297eba212f67e1d637e1fa7620c5aa1db54c
Emil Rühmland <emilrueh@googlemail.com>
2023-10-10 11:24:10 +0200

    Fix remote driver
    Implementing @ton77v commit 5c3fe7b which fixed the executable_path TypeError. Fixing remote driver functionality by fixing the deprecated desired_capabilities for the up-to-date 'options' version. Also updated the readme slightly for readability and better understanding of the settings for remote drivers

--- scrapy_selenium/middlewares.py
+++ scrapy_selenium/middlewares.py
@@ -13,8 +13,14 @@ from .http import SeleniumRequest
 class SeleniumMiddleware:
     """Scrapy middleware handling the requests using selenium"""
 
-    def __init__(self, driver_name, driver_executable_path,
-        browser_executable_path, command_executor, driver_arguments):
+    def __init__(
+        self,
+        driver_name,
+        driver_executable_path,
+        browser_executable_path,
+        command_executor,
+        driver_arguments,
+    ):
         """Initialize the selenium webdriver
 
         Parameters
@@ -31,13 +37,13 @@ class SeleniumMiddleware:
             Selenium remote server endpoint
         """
 
-        webdriver_base_path = f'selenium.webdriver.{driver_name}'
+        webdriver_base_path = f"selenium.webdriver.{driver_name}"
 
-        driver_klass_module = import_module(f'{webdriver_base_path}.webdriver')
-        driver_klass = getattr(driver_klass_module, 'WebDriver')
+        driver_klass_module = import_module(f"{webdriver_base_path}.webdriver")
+        driver_klass = getattr(driver_klass_module, "WebDriver")
 
-        driver_options_module = import_module(f'{webdriver_base_path}.options')
-        driver_options_klass = getattr(driver_options_module, 'Options')
+        driver_options_module = import_module(f"{webdriver_base_path}.options")
+        driver_options_klass = getattr(driver_options_module, "Options")
 
         driver_options = driver_options_klass()
 
@@ -47,47 +53,65 @@ class SeleniumMiddleware:
             driver_options.add_argument(argument)
 
         driver_kwargs = {
-            'executable_path': driver_executable_path,
-            f'{driver_name}_options': driver_options
+            "executable_path": driver_executable_path,
+            f"{driver_name}_options": driver_options,
         }
 
         # locally installed driver
         if driver_executable_path is not None:
             driver_kwargs = {
-                'executable_path': driver_executable_path,
-                f'{driver_name}_options': driver_options
+                "executable_path": driver_executable_path,
+                f"{driver_name}_options": driver_options,
             }
             self.driver = driver_klass(**driver_kwargs)
         # remote driver
         elif command_executor is not None:
             from selenium import webdriver
-            capabilities = driver_options.to_capabilities()
-            self.driver = webdriver.Remote(command_executor=command_executor,
-                                           desired_capabilities=capabilities)
+
+            self.driver = webdriver.Remote(
+                command_executor=command_executor, options=driver_options
+            )
+        # webdriver-manager
+        else:
+            # selenium4+ & webdriver-manager
+            from selenium import webdriver
+            from webdriver_manager.chrome import ChromeDriverManager
+            from selenium.webdriver.chrome.service import Service as ChromeService
+
+            if driver_name and driver_name.lower() == "chrome":
+                # options = webdriver.ChromeOptions()
+                # options.add_argument(o)
+                self.driver = webdriver.Chrome(
+                    options=driver_options, service=ChromeService(ChromeDriverManager().install())
+                )
 
     @classmethod
     def from_crawler(cls, crawler):
         """Initialize the middleware with the crawler settings"""
 
-        driver_name = crawler.settings.get('SELENIUM_DRIVER_NAME')
-        driver_executable_path = crawler.settings.get('SELENIUM_DRIVER_EXECUTABLE_PATH')
-        browser_executable_path = crawler.settings.get('SELENIUM_BROWSER_EXECUTABLE_PATH')
-        command_executor = crawler.settings.get('SELENIUM_COMMAND_EXECUTOR')
-        driver_arguments = crawler.settings.get('SELENIUM_DRIVER_ARGUMENTS')
+        driver_name = crawler.settings.get("SELENIUM_DRIVER_NAME")
+        driver_executable_path = crawler.settings.get("SELENIUM_DRIVER_EXECUTABLE_PATH")
+        browser_executable_path = crawler.settings.get("SELENIUM_BROWSER_EXECUTABLE_PATH")
+        command_executor = crawler.settings.get("SELENIUM_COMMAND_EXECUTOR")
+        driver_arguments = crawler.settings.get("SELENIUM_DRIVER_ARGUMENTS")
 
         if driver_name is None:
-            raise NotConfigured('SELENIUM_DRIVER_NAME must be set')
+            raise NotConfigured("SELENIUM_DRIVER_NAME must be set")
 
-        if driver_executable_path is None and command_executor is None:
-            raise NotConfigured('Either SELENIUM_DRIVER_EXECUTABLE_PATH '
-                                'or SELENIUM_COMMAND_EXECUTOR must be set')
+        # let's use webdriver-manager when nothing specified instead | RN just for Chrome
+        if (driver_name.lower() != "chrome") and (
+            driver_executable_path is None and command_executor is None
+        ):
+            raise NotConfigured(
+                "Either SELENIUM_DRIVER_EXECUTABLE_PATH " "or SELENIUM_COMMAND_EXECUTOR must be set"
+            )
 
         middleware = cls(
             driver_name=driver_name,
             driver_executable_path=driver_executable_path,
             browser_executable_path=browser_executable_path,
             command_executor=command_executor,
-            driver_arguments=driver_arguments
+            driver_arguments=driver_arguments,
         )
 
         crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
@@ -103,20 +127,13 @@ class SeleniumMiddleware:
         self.driver.get(request.url)
 
         for cookie_name, cookie_value in request.cookies.items():
-            self.driver.add_cookie(
-                {
-                    'name': cookie_name,
-                    'value': cookie_value
-                }
-            )
+            self.driver.add_cookie({"name": cookie_name, "value": cookie_value})
 
         if request.wait_until:
-            WebDriverWait(self.driver, request.wait_time).until(
-                request.wait_until
-            )
+            WebDriverWait(self.driver, request.wait_time).until(request.wait_until)
 
         if request.screenshot:
-            request.meta['screenshot'] = self.driver.get_screenshot_as_png()
+            request.meta["screenshot"] = self.driver.get_screenshot_as_png()
 
         if request.script:
             self.driver.execute_script(request.script)
@@ -124,17 +141,11 @@ class SeleniumMiddleware:
         body = str.encode(self.driver.page_source)
 
         # Expose the driver via the "meta" attribute
-        request.meta.update({'driver': self.driver})
+        request.meta.update({"driver": self.driver})
 
-        return HtmlResponse(
-            self.driver.current_url,
-            body=body,
-            encoding='utf-8',
-            request=request
-        )
+        return HtmlResponse(self.driver.current_url, body=body, encoding="utf-8", request=request)
 
     def spider_closed(self):
         """Shutdown the driver when spider is closed"""
 
         self.driver.quit()
-
