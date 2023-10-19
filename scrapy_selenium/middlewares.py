97cf63bba6d331871454ac77b0960e86ad6cd03c
Emil Rühmland <emilrueh@googlemail.com>
2023-10-19 14:47:02 +0200

    Implement retry on WebDriverException

--- scrapy_selenium/middlewares.py
+++ scrapy_selenium/middlewares.py
@@ -1,18 +1,22 @@
-"""This module contains the ``SeleniumMiddleware`` scrapy middleware"""
-
 from importlib import import_module
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import WebDriverException
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 from scrapy import signals
 from scrapy.exceptions import NotConfigured
 from scrapy.http import HtmlResponse
-from selenium.webdriver.support.ui import WebDriverWait
+from .http import SeleniumRequest  # Assuming SeleniumRequest is in http.py in the same folder
+import time
 
-from .http import SeleniumRequest
+import logging
+
+selenium_logger = logging.getLogger("selenium.webdriver.remote.remote_connection")
+selenium_logger.setLevel(logging.INFO)
 
 
 class SeleniumMiddleware:
-    """Scrapy middleware handling the requests using selenium"""
-
     def __init__(
         self,
         driver_name,
@@ -21,131 +25,122 @@ class SeleniumMiddleware:
         command_executor,
         driver_arguments,
     ):
-        """Initialize the selenium webdriver
+        self.driver_name = driver_name
+        self.driver_executable_path = driver_executable_path
+        self.browser_executable_path = browser_executable_path
+        self.command_executor = command_executor
+        self.driver_arguments = driver_arguments
+        self._initialize_driver()
+        self.retry_count = 0
 
-        Parameters
-        ----------
-        driver_name: str
-            The selenium ``WebDriver`` to use
-        driver_executable_path: str
-            The path of the executable binary of the driver
-        driver_arguments: list
-            A list of arguments to initialize the driver
-        browser_executable_path: str
-            The path of the executable binary of the browser
-        command_executor: str
-            Selenium remote server endpoint
-        """
+    def _initialize_driver(self):
+        retries = 3
+        for i in range(retries):
+            try:
+                webdriver_base_path = f"selenium.webdriver.{self.driver_name}"
+                driver_klass_module = import_module(f"{webdriver_base_path}.webdriver")
+                driver_klass = getattr(driver_klass_module, "WebDriver")
+                driver_options_module = import_module(f"{webdriver_base_path}.options")
+                driver_options_klass = getattr(driver_options_module, "Options")
 
-        webdriver_base_path = f"selenium.webdriver.{driver_name}"
+                driver_options = driver_options_klass()
+                if self.browser_executable_path:
+                    driver_options.binary_location = self.browser_executable_path
+                for argument in self.driver_arguments:
+                    driver_options.add_argument(argument)
 
-        driver_klass_module = import_module(f"{webdriver_base_path}.webdriver")
-        driver_klass = getattr(driver_klass_module, "WebDriver")
+                driver_kwargs = {
+                    "executable_path": self.driver_executable_path,
+                    f"{self.driver_name}_options": driver_options,
+                }
 
-        driver_options_module = import_module(f"{webdriver_base_path}.options")
-        driver_options_klass = getattr(driver_options_module, "Options")
-
-        driver_options = driver_options_klass()
-
-        if browser_executable_path:
-            driver_options.binary_location = browser_executable_path
-        for argument in driver_arguments:
-            driver_options.add_argument(argument)
-
-        driver_kwargs = {
-            "executable_path": driver_executable_path,
-            f"{driver_name}_options": driver_options,
-        }
-
-        # locally installed driver
-        if driver_executable_path is not None:
-            driver_kwargs = {
-                "executable_path": driver_executable_path,
-                f"{driver_name}_options": driver_options,
-            }
-            self.driver = driver_klass(**driver_kwargs)
-        # remote driver
-        elif command_executor is not None:
-            from selenium import webdriver
-
-            self.driver = webdriver.Remote(
-                command_executor=command_executor, options=driver_options
-            )
-        # webdriver-manager
-        else:
-            # selenium4+ & webdriver-manager
-            from selenium import webdriver
-            from webdriver_manager.chrome import ChromeDriverManager
-            from selenium.webdriver.chrome.service import Service as ChromeService
-
-            if driver_name and driver_name.lower() == "chrome":
-                # options = webdriver.ChromeOptions()
-                # options.add_argument(o)
-                self.driver = webdriver.Chrome(
-                    options=driver_options, service=ChromeService(ChromeDriverManager().install())
-                )
+                if self.driver_executable_path is not None:
+                    self.driver = driver_klass(**driver_kwargs)
+                elif self.command_executor is not None:
+                    self.driver = webdriver.Remote(
+                        command_executor=self.command_executor, options=driver_options
+                    )
+                else:
+                    if self.driver_name and self.driver_name.lower() == "chrome":
+                        self.driver = webdriver.Chrome(
+                            options=driver_options,
+                            service=ChromeService(ChromeDriverManager().install()),
+                        )
+                break
+            except WebDriverException:
+                if i < retries - 1:  # not the last retry
+                    print(
+                        f"Encountered WebDriverException during driver initialization. Retrying... ({i+1})"
+                    )
+                    time.sleep(2**i)  # exponential backoff
+                else:
+                    print("Max retries reached. Could not initialize the driver.")
+                    raise  # re-raise the exception
 
     @classmethod
     def from_crawler(cls, crawler):
-        """Initialize the middleware with the crawler settings"""
-
         driver_name = crawler.settings.get("SELENIUM_DRIVER_NAME")
         driver_executable_path = crawler.settings.get("SELENIUM_DRIVER_EXECUTABLE_PATH")
         browser_executable_path = crawler.settings.get("SELENIUM_BROWSER_EXECUTABLE_PATH")
         command_executor = crawler.settings.get("SELENIUM_COMMAND_EXECUTOR")
         driver_arguments = crawler.settings.get("SELENIUM_DRIVER_ARGUMENTS")
 
-        if driver_name is None:
+        if not driver_name:
             raise NotConfigured("SELENIUM_DRIVER_NAME must be set")
 
-        # let's use webdriver-manager when nothing specified instead | RN just for Chrome
-        if (driver_name.lower() != "chrome") and (
-            driver_executable_path is None and command_executor is None
-        ):
-            raise NotConfigured(
-                "Either SELENIUM_DRIVER_EXECUTABLE_PATH " "or SELENIUM_COMMAND_EXECUTOR must be set"
-            )
-
         middleware = cls(
-            driver_name=driver_name,
-            driver_executable_path=driver_executable_path,
-            browser_executable_path=browser_executable_path,
-            command_executor=command_executor,
-            driver_arguments=driver_arguments,
+            driver_name,
+            driver_executable_path,
+            browser_executable_path,
+            command_executor,
+            driver_arguments,
         )
 
         crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
-
         return middleware
 
     def process_request(self, request, spider):
-        """Process a request using the selenium driver if applicable"""
+        try:
+            if not isinstance(request, SeleniumRequest):
+                return None
 
-        if not isinstance(request, SeleniumRequest):
-            return None
+            # OPENING WEBSITE
+            self.driver.get(request.url)
 
-        self.driver.get(request.url)
+            for cookie_name, cookie_value in request.cookies.items():
+                self.driver.add_cookie({"name": cookie_name, "value": cookie_value})
 
-        for cookie_name, cookie_value in request.cookies.items():
-            self.driver.add_cookie({"name": cookie_name, "value": cookie_value})
+            if request.wait_until:
+                WebDriverWait(self.driver, request.wait_time).until(request.wait_until)
 
-        if request.wait_until:
-            WebDriverWait(self.driver, request.wait_time).until(request.wait_until)
+            if request.screenshot:
+                request.meta["screenshot"] = self.driver.get_screenshot_as_png()
 
-        if request.screenshot:
-            request.meta["screenshot"] = self.driver.get_screenshot_as_png()
+            if request.script:
+                self.driver.execute_script(request.script)
 
-        if request.script:
-            self.driver.execute_script(request.script)
+            body = str.encode(self.driver.page_source)
+            request.meta.update({"driver": self.driver})
 
-        body = str.encode(self.driver.page_source)
+            processed_request = HtmlResponse(
+                self.driver.current_url, body=body, encoding="utf-8", request=request
+            )
+            self.retry_count = 0  # Reset the retry counter if successful
+            return processed_request
 
-        # Expose the driver via the "meta" attribute
-        request.meta.update({"driver": self.driver})
-
-        return HtmlResponse(self.driver.current_url, body=body, encoding="utf-8", request=request)
+        except WebDriverException:
+            if self.retry_count < 3:  # Maximum retry limit
+                print(
+                    f"Encountered WebDriverException with {request.url}\nRetrying in {2 ** self.retry_count}s..."
+                )
+                self.retry_count += 1
+                time.sleep(2**self.retry_count)  # Exponential backoff
+                request.meta["retrying"] = True
+                self._initialize_driver()
+                return request
+            else:
+                self.retry_count = 0  # Reset the retry counter
+                raise  # Reraise the exception if maximum retries reached
 
     def spider_closed(self):
-        """Shutdown the driver when spider is closed"""
-
         self.driver.quit()
