da2e95874fc6c736d8d00184b47566b2443e0c73
Emil Rühmland <emilrueh@googlemail.com>
2023-10-19 15:13:51 +0200

    Modernizing setup.py

--- setup.py
+++ setup.py
@@ -1,34 +1,10 @@
-"""This module contains the packaging routine for the pybook package"""
-
 from setuptools import setup, find_packages
-try:
-    from pip.download import PipSession
-    from pip.req import parse_requirements
-except ImportError:
-    # It is quick hack to support pip 10 that has changed its internal
-    # structure of the modules.
-    from pip._internal.download import PipSession
-    from pip._internal.req.req_file import parse_requirements
 
 
 def get_requirements(source):
-    """Get the requirements from the given ``source``
-
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
+    with open(source, "r") as f:
+        requirements = f.read().splitlines()
+    return requirements
 
 
-setup(
-    packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
-)
-
-
+setup(packages=find_packages(), install_requires=get_requirements("requirements/requirements.txt"))
